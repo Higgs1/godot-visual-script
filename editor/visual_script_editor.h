33496fd1ba840381dd3b5cc179b5473137fce51f
Lexxy Fox <lexxyfox@gmail.com>
2022-10-29 16:24:54 -0500

    Compiles with Godot 4 successfully.

--- editor/visual_script_editor.h
+++ editor/visual_script_editor.h
@@ -356,6 +356,7 @@ public:
 	virtual void validate() override;
 
 	virtual Control *get_base_editor() const override;
+	virtual Variant get_navigation_state() override { return NULL; }
 
 	static void register_editor();
 
